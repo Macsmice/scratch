fac1035c1fed7781d5e3dc28e48aa0a5b235d7c7
Cristina Solana <cs@nightshiftcreative.com>
2011-12-17 02:23:15 -0500

    php includes, just for testing patterns separately

---
+++ includes/bottom.php
@@ -0,0 +1,7 @@
+	<!-- Combine and Compress These JS Files -->
+	<script type="text/javascript" charset="utf-8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="js/jquery.alerts.js"></script>
+	<!-- End Combine and Compress These JS Files -->
+	<!-- <script type="text/javascript" charset="utf-8" src="../js/all.js"></script> -->
+	</body>
+</html>
---
+++ includes/top.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--[if lte IE 7]>	<![endif]-->
+<!--[if IE 7]>		<![endif]-->
+<!--[if IE 8]>		<![endif]-->
+<!--[if (gt IE 9)|!(IE)]> <!--> <html xml:lang="en" lang="en" class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>Scratch: The Agnostic</title>
+		<meta name="description" content="Describe me." />
+		<!-- NOTE: Uncomment to force Chrome frame for IE or simply delete
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		-->
+
+		<!-- from Foundation build script -->
+		<!-- Combine and Compress These CSS Files -->
+		<link rel="stylesheet" href="css/resets.css">
+		<link rel="stylesheet" href="css/screen.css">
+		<link rel="stylesheet" href="css/grid.css">
+		<link rel="stylesheet" href="css/forms.css">
+		<link rel="stylesheet" href="css/alert.css">
+		<link rel="stylesheet" href="css/button.css">
+		<link rel="stylesheet" href="css/nav.css">
+		<link rel="stylesheet" href="css/pagination.css">
+		<link rel="stylesheet" href="css/tabs.css">
+		<!-- End Combine and Compress These CSS Files -->
+		<!-- <link rel="stylesheet" href="../stylesheets/all.css?v=2"> -->
+
+		<!-- NOTE: Plan on supporting IE lt 9 beyond serving it content? Or fight the good fight and delete it. -->
+		<!--[if lt IE 9]>
+			<link rel="stylesheet" href="stylesheets/ie.css">
+		<![endif]-->
+
+		<!-- NOTE: Leave this for IE lt 9 to recognize HTML5 elements. Or if you fancy feature detection + polyfills - delete this, use Modernzr. -->
+		<!-- IE Fix for HTML5 Tags -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!--<script src="js/modernizr.custom.js"></script>-->
+	</head>
+	<body>
