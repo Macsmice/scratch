8f0ad797f789020189860dd871815f642db3c37b
Cristina Solana <cs@nightshiftcreative.com>
2011-12-17 02:22:23 -0500

    buttons as anchor, button or input element

---
+++ button.php
@@ -0,0 +1,5 @@
+<?php include("includes/top.php"); ?>
+	<a href="#" class="button">anchor</a>
+	<button type="button" class="button">button</button>
+	<input type="button" value="input" class="button">
+<?php include("includes/bottom.php"); ?>
