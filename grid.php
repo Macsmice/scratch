379d9078c050a4873ed6bc5fd3beeda89c3bd7b2
Cristina Solana <cs@nightshiftcreative.com>
2011-12-17 02:28:28 -0500

    Responsive and semantic grid, use any html5 (or other) tag you want, plug in some numbers and class names

---
+++ grid.php
@@ -0,0 +1,70 @@
+<?php include("includes/top.php"); ?>
+	<div class="grid">
+		<header class="group">
+			<div class="span-12">
+				<p>12</p>
+			</div>
+			<div class="span-12">
+				<p>12</p>
+			</div>
+		</header>
+
+		<nav class="group">
+			<article class="span-3">
+				<p>3</p>
+			</article>
+			<aside class="span-5">
+				<p>5</p>
+			</aside>
+			<article class="span-3">
+				<p>3</p>
+			</article>
+			<aside class="span-5">
+				<p>5</p>
+			</aside>
+			<article class="span-3">
+				<p>3</p>
+			</article>
+			<aside class="span-5">
+				<p>5</p>
+			</aside>
+		</nav>
+
+		<section class="group">
+			<article class="span-8">
+				<p>8</p>
+			</article>
+			<aside class="span-8">
+				<p>8</p>
+			</aside>
+			<article class="span-8">
+				<p>8</p>
+			</article>
+		</section>
+
+		<section class="group">
+			<article class="span-6">
+				<p>6</p>
+			</article>
+			<aside class="span-6">
+				<p>6</p>
+			</aside>
+			<article class="span-6">
+				<p>6</p>
+			</article>
+			<aside class="span-6">
+				<p>6</p>
+			</aside>
+		</section>
+
+		<footer class="group">
+			<article class="span-6">
+				<p>18</p>
+			</article>
+			<aside class="span-18">
+				<p>6</p>
+			</aside>
+		</footer>
+
+	</div>
+<?php include("includes/bottom.php"); ?>
